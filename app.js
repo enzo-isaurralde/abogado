ac6926532a637682e2e134e1dbb10aa66d80c974
Enzo Ariel Isaurralde <enzoarielisaurralde@gmail.com>
2026-04-12 16:40:29 -0300

    Número cambiado

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function enviarWhatsApp() {
   const nombre = document.getElementById('nombre').value;
   const mensaje = document.getElementById('mensaje').value;
-  const telefonoAbogado = "5491157688472"; // Formato internacional para Argentina
+  const telefonoAbogado = "5491151503610"; // Formato internacional para Argentina
 
   const url = `https://wa.me/${telefonoAbogado}?text=Hola,%20mi%20nombre%20es%20${nombre}.%20Consulta:%20${mensaje}`;
   
